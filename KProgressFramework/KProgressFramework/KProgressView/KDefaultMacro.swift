2a47d61c091bd51174cdcb47580df5a08b3440da
“questerMan  sdssdsdgit config --global user.name “questerMan <luyikun01@163.com>
2020-09-07 15:56:18 +0800

    tag=1.0.1

--- KProgressFramework/KProgressFramework/KProgressView/KDefaultMacro.swift
+++ KProgressFramework/KProgressFramework/KProgressView/KDefaultMacro.swift
@@ -11,13 +11,13 @@ import UIKit
 let KScreen_Width = UIScreen.main.bounds.width
 let KScreen_Height = UIScreen.main.bounds.height
 
-//iphone6s为适配的宽度尺寸比例
+// iphone6s为适配的宽度尺寸比例
 func kScaleWidth(_ width:CGFloat) -> CGFloat { return ((width)*(KScreen_Width/375.0))}
-
+// 判断是否iphone6P
 let IsIphone6P = (KScreen_Width == 414.0 ? true : false)
 
 let KSizeScale = (IsIphone6P ? 1.2 : 1)
-//字体适配
+// 字体适配
 func kFontSize(_ value:CGFloat) -> CGFloat{ return value * CGFloat(KSizeScale)}
 
 class KDefaultMacro: NSObject {
